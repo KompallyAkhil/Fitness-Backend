2d9989dd169e8ee84af2dd64b75ce8c0eb8aad7d
Akki-luffy <akhilkompally27@gmail.com>
2024-11-16 16:36:39 +0530

    Updated the error msg

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post('/Register', async (req, res) => {
 app.post('/SignIn', async (req,res)=>{
     const {Username,Password} = req.body;
     if(!Username || !Password){
-        return res.status(409).json({message : "Usaername or Password cannot be empty"})
+        return res.status(409).json({message : "Username or Password cannot be empty"})
     }
     const findUser = await Login.findOne({Username});
     if(!findUser){
