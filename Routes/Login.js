fa9a24f5296ecd2814f34065f186c1afcbf41919
Akki-luffy <akhilkompally27@gmail.com>
2025-01-11 18:15:45 +0530

    Added JWT token

--- Routes/Login.js
+++ Routes/Login.js
@@ -1,7 +1,9 @@
 import express from "express";
 import Login from "../model/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const router = express.Router();
+const SECRET_KEY = "@#$%#&*(^$%^&*";
 router.post('/', async(req,res) => {
     const {Username,Password} = req.body;
     if(!Username || !Password){
@@ -12,11 +14,10 @@ router.post('/', async(req,res) => {
         return res.status(409).json({message:"User not found"});
     }
     const findPassword = await bcrypt.compare(Password,findUser.Password);
-    if(findPassword){
-        return res.json({loginUsername : Username,loginStatus:true});
+    if(!findPassword){
+        return res.status(409).json({message:"Invalid Password"});
     }
-    else{
-        return res.status(409).json({message:"Incorrect Password"});
-    }
+    const token = jwt.sign({Username},SECRET_KEY,{expiresIn:"1hr"});
+    res.json({token,loginStatus:true});
 })
 export default router;
